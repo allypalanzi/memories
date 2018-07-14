3879a703aae8d0795ca7a557e8989022b6b5e0bd
Ally Palanzi <ampalanzi@gmail.com>
2018-07-14 09:37:21 -0400

    oops, forgot to add these

---
+++ src/components/Button/Button.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import styles from './Button.scss';
+
+const Button = (props) => {
+  const { onClick, text } = props;
+  return <button className={styles.button} onClick={onClick}>{text}</button>
+}
+
+export default Button;
