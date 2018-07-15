054f9a4280cef6a1eb004a272ef2a5b2da09bcd3
Ally Palanzi <ampalanzi@gmail.com>
2018-07-15 11:55:23 -0400

    refactor css

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -1,11 +1,12 @@
 import React from 'react'
-import styles from './index.scss'
 import Game from '../Game/Game'
 
+import '../../client/styles/global.scss';
+
 const App = () => (
-  <main className={styles.app} role='main'>
-    <h1 className={styles.header}>✨ Memory ✨</h1>
-    <p className={styles.body}>Select a card and find it's pair!</p>
+  <main className='l-wrapper' role='main'>
+    <h1>✨ Memory ✨</h1>
+    <p>Select a card and find it's pair!</p>
     <Game />
   </main>
 )
