ade99902f157381457da85f84f4eaed11723ba71
Ally Palanzi <ampalanzi@gmail.com>
2018-07-15 14:54:47 -0400

    add button test

--- src/components/Button/Button.test.js
+++ src/components/Button/Button.test.js
@@ -4,8 +4,9 @@ import { shallow } from 'enzyme';
 import Button from './Button.js';
 
 describe('<Button/ >', () => {
+  const onClick = jest.fn();
   const props = {
-    onClick: jest.fn(),
+    onClick,
     text: 'I am a button, click me'
   }
 
@@ -13,5 +14,10 @@ describe('<Button/ >', () => {
 
   it('renders correctly', () => {
     expect(renderedComponent).toMatchSnapshot();
+  });
+
+  it('calls the function on click', () => {
+    renderedComponent.simulate('click');
+    expect(onClick).toHaveBeenCalled();
   });
 });
