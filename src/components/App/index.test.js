ae30e4f648586a472735dc31235836a1d09d55a1
Ally Palanzi <ampalanzi@gmail.com>
2018-07-15 18:26:28 -0400

    linting

--- src/components/App/index.test.js
+++ src/components/App/index.test.js
@@ -1,20 +1,20 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import React from 'react'
+import ReactDOM from 'react-dom'
 import { shallow } from 'enzyme'
 import toJson from 'enzyme-to-json'
-import App from './';
+import App from './'
 
 jest.mock('../../client/services')
 
 describe('<App/ >', () => {
-  const renderedComponent = shallow(<App />);
+  const renderedComponent = shallow(<App />)
 
   it('renders without crashing', () => {
     const div = document.createElement('div')
     ReactDOM.render(<App />, div)
-  });
+  })
 
   it('renders correctly', () => {
-    expect(toJson(renderedComponent)).toMatchSnapshot();
-  });
-});
+    expect(toJson(renderedComponent)).toMatchSnapshot()
+  })
+})
